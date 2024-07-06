b2238b669822eb0e52fdc063bd26a9cc03bdebab
Ram <ram@gmail.com>
2024-07-07 00:36:22 +0530

    test

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ const addDataToHTML = () => {
                 <h2>${product.name}</h2>
                 <div class="price">₹<strike>${product.mrpprice}</strike>  ₹${product.price}</div>
                 <button class="addCart" id="open-popup">Add To Cart</button>
-                <div id="popup"><h5>ADDED ${product.name}</h5></div>
+                <div id="popup"><h5>ADDED</h5></div>
 			`;
             listProductHTML.appendChild(newProduct);
         })
@@ -44,6 +44,8 @@ listProductHTML.addEventListener('click', (event) => {
     if(positionClick.classList.contains('addCart')){
         let id_product = positionClick.parentElement.dataset.id;
         addToCart(id_product);
+        popup.classList.add("show"); 
+        setTimeout(() => {popup.classList.remove("show");}, 800);
     }
 })
 
